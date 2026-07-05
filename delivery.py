ec7f3c2837beb8ac0597b20bc02135846097c258
bruno <brunorehling91@gmail.com>
2026-07-05 20:43:52 -0300

    retornando o arquivo delivery.py para a funcionalidade original

--- delivery.py
+++ delivery.py
@@ -1,11 +1,10 @@
 
 
 def calculate_delivery(items: list[float], distance: float) -> float:
-#    delivery_tax : float = 0
+    delivery_tax : float = 0
 
-#    delivery_value = distance * 0.5
-#    if delivery_value > 2 :
-#        delivery_tax = delivery_value
+    delivery_value = distance * 0.5
+    if delivery_value > 2 :
+        delivery_tax = delivery_value
 
-#    return sum(items, delivery_tax)
-    return 0
+    return sum(items, delivery_tax)
